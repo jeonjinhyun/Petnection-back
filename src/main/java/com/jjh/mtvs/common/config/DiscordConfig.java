c9a82baffaf6b0ce12eb96e2510db9c914b0854e
jeonjinhyun <jjh06500@gmail.com>
2024-11-13 18:08:49 +0900

    1113 save

--- src/main/java/com/jjh/mtvs/common/config/DiscordConfig.java
+++ src/main/java/com/jjh/mtvs/common/config/DiscordConfig.java
@@ -1,21 +1,21 @@
-package com.jjh.mtvs.common.config;
-
-import discord4j.core.DiscordClientBuilder;
-import discord4j.core.GatewayDiscordClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DiscordConfig {
-    @Value("${discord.token}")
-    private String token;
-
-    @Bean
-    public GatewayDiscordClient gatewayDiscordClient() {
-        return DiscordClientBuilder.create(token)
-                .build()
-                .login()
-                .block();
-    }
-}
+//package com.jjh.mtvs.common.config;
+//
+//import discord4j.core.DiscordClientBuilder;
+//import discord4j.core.GatewayDiscordClient;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DiscordConfig {
+//    @Value("${discord.token}")
+//    private String token;
+//
+//    @Bean
+//    public GatewayDiscordClient gatewayDiscordClient() {
+//        return DiscordClientBuilder.create(token)
+//                .build()
+//                .login()
+//                .block();
+//    }
+//}
